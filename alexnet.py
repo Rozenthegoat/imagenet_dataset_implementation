da5fb277d99ea25eba150df6a50e811c4739060e
Rozenthegoat <rozenthegoat@qq.com>
2023-10-05 21:11:45 +0800

    update alexnet

---
+++ alexnet.py
@@ -0,0 +1,53 @@
+import os
+import torch
+import torch.nn as nn
+from torch.utils import data
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"device: {device}")
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=1000, dropout: float = 0.5):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2)
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+net = AlexNet()
+print(net)
+
+
